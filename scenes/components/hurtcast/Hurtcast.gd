aa63729e77c098ba07f942e5b78dba6de9525497
Kevin Sánchez Obando <kesanchezo@gmail.com>
2025-01-26 23:10:42 -0600

    trenchbroom config

--- scenes/components/hurtcast/Hurtcast.gd
+++ scenes/components/hurtcast/Hurtcast.gd
@@ -5,6 +5,7 @@ class_name Hurtcast
 @export var damage := 0.0
 @export_range(1, 100) var number_of_rays := 1
 @export_range(1.0, 10000, 0.1) var cast_range := 25.0
+@export_range(0.0, 1.0, 0.001) var spread := 0.0
 @export_flags_3d_physics var ray_cast_mask: int
 
 @onready var directional_node := $DirectionalNode as Node3D
@@ -31,7 +32,7 @@ func fire_rays() -> Array[Dictionary]:
 func cast_ray() -> Dictionary:
 	var space_state := get_world_3d().direct_space_state
 	
-	var adjusted_target_position := direction_node.global_position + get_random_spread_vector()
+	var adjusted_target_position := directional_node.global_position + get_random_spread_vector()
 	
 	var direction := (adjusted_target_position - global_position).normalized()
 	
@@ -42,3 +43,11 @@ func cast_ray() -> Dictionary:
 	)
 	query.collide_with_areas = true
 	return space_state.intersect_ray(query)
+
+
+func get_random_spread_vector() -> Vector3:
+	return Vector3(
+			randf_range(-spread, spread), 
+			randf_range(-spread, spread), 
+			randf_range(-spread, spread)
+	)
