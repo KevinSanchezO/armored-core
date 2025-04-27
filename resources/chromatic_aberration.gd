c1efb130e2cd249404c6e2bc7f992a25b4c1b48a
Kevin Sánchez Obando <kesanchezo@gmail.com>
2025-04-27 09:10:33 -0600

    slow motion shader transition

--- resources/chromatic_aberration.gd
+++ resources/chromatic_aberration.gd
@@ -1,18 +1,31 @@
 extends CanvasLayer
 class_name ChromaticAberration
 
+@export var strength_shader := 0.4
+
 @onready var shader_color_rect := $Control/ColorRect
 
 
 func _ready() -> void:
-	shader_color_rect.visible = false
 	SlowMotion.slow_motion_started.connect(_show_chromatic_aberration)
 	SlowMotion.slow_motion_ended.connect(_hide_chromatic_aberration)
 
 
 func _show_chromatic_aberration() -> void:
-	shader_color_rect.visible = true
+	var tween = create_tween()
+	tween.tween_method(_update_shader_strength, _get_current_strength(), strength_shader, 0.5)
+	#shader_color_rect.material.set("shader_parameter/strength", 0.5)
 
 
 func _hide_chromatic_aberration() -> void:
-	shader_color_rect.visible = false
+	var tween = create_tween()
+	tween.tween_method(_update_shader_strength, _get_current_strength(), 0.0, 0.5)
+	#shader_color_rect.material.set("shader_parameter/strength", 0)
+
+
+func _update_shader_strength(value: float) -> void:
+	shader_color_rect.material.set("shader_parameter/strength", value)
+
+
+func _get_current_strength() -> float:
+	return shader_color_rect.material.get("shader_parameter/strength")
